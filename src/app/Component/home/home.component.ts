da9f129d1603c936475691a84154ac463a8aa0a7
Omar <omarelaiouti@gmail.com>
2024-03-20 07:37:19 +0200

    CouesesSlider

--- src/app/Component/home/home.component.ts
+++ src/app/Component/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CourcesComponent } from '../cources/cources.component';
+import { CourseSliderComponent } from "../CourseSlider/course-slider/course-slider.component";
 
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [CourcesComponent],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [CourcesComponent, CourseSliderComponent]
 })
 export class HomeComponent {
 
