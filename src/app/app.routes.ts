a399a1e6fa459d917255983070ba3d85668b81f7
Omar <omarelaiouti@gmail.com>
2024-03-20 23:58:04 +0200

    Clear

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,10 +8,7 @@ import { CartComponent } from './Component/cart/cart.component';
 import { AboutusComponent } from './Component/aboutus/aboutus.component';
 import { ContactusComponent } from './Component/contactus/contactus.component';
 import { TechOnUdemyComponent } from './Component/tech-on-udemy/tech-on-udemy.component';
-<<<<<<< HEAD
 import { SupportComponent } from './Component/support/support.component';
-=======
->>>>>>> c3bbbab70f00f3925282c1e1403bb94030adfc26
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, title: 'home' },
@@ -21,13 +18,9 @@ export const routes: Routes = [
   { path: 'cart', component: CartComponent, title: 'cart' },
   { path: 'aboutus', component: AboutusComponent, title: 'aboutus' },
   { path: 'contactus', component: ContactusComponent, title: 'contactus' },
-<<<<<<< HEAD
   { path: 'techOnUdemy', component: TechOnUdemyComponent, title: 'techOnUdemy' },
   { path: 'support', component: SupportComponent, title: 'support' }
 
-=======
-  { path: 'techOnUdemy', component: TechOnUdemyComponent, title: 'techOnUdemy' }
->>>>>>> c3bbbab70f00f3925282c1e1403bb94030adfc26
 
 
 ];
