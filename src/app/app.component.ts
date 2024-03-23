d4cee49cfb6b7beb7fc6ee435612ede0259cde9d
Doha <dohashouman12345@gmail.com>
2024-03-23 22:38:06 +0200

    course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,16 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './Component/footer/footer.component';
 import { NavbarComponent } from './Component/navbar/navbar.component';
 import { HomeComponent } from './Component/home/home.component';
+import { CourseDetailsComponent } from "./Component/course-details/course-details.component";
+
 
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,FooterComponent,NavbarComponent,HomeComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, FooterComponent, NavbarComponent, HomeComponent, CourseDetailsComponent]
 })
 export class AppComponent {
   title = 'graduation_proj';
