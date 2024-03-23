7fc328b2d362c6ac534ee5768a5d9a4ec249bbcf
sohilafouad <sohilafouad11@gmail.com>
2024-03-23 23:14:15 +0200

    account&mylearning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './Component/footer/footer.component';
 import { NavbarComponent } from './Component/navbar/navbar.component';
 import { HomeComponent } from './Component/home/home.component';
+import { MyLearningComponent } from './Component/my-learning/my-learning.component';
+import { AccountProfileComponent } from './Component/account-profile/account-profile.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FooterComponent,NavbarComponent,HomeComponent],
+  imports: [RouterOutlet,FooterComponent,NavbarComponent,HomeComponent,MyLearningComponent,AccountProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
